a5d07bdc6b2efbc67e842c65ebbd21ce1cbeae7b
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-11-10 21:41:02 +0900

    Add --push to buildx

--- client/attiude-est/buildx.sh
+++ client/attiude-est/buildx.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/usr/local/bin/docker buildx build . -t tetsuzawa/ahrs-pi:0.0.2 --platform linux/arm64/v8
+/usr/local/bin/docker buildx build . -t tetsuzawa/ahrs-pi:0.0.2 --platform linux/arm64/v8 --push
