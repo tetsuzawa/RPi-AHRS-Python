26fa6b670b9b53ca68e3d3a474c2ebe96754ffb6
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-11-11 12:06:36 +0900

    Fix Dockerfile

--- client/attiude-est/buildx.sh
+++ client/attiude-est/buildx.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-/usr/local/bin/docker buildx build . -t tetsuzawa/ahrs-pi:${1} --platform=linux/arm/v7
+/usr/local/bin/docker buildx build . -t tetsuzawa/ahrs-pi:${1} --platform=linux/arm/v7 --no-cache=true
 /usr/local/bin/docker push tetsuzawa/ahrs-pi:${1}
