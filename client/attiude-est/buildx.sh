b6cfb3d092b1ba701d570e7ab93c4ad30fb12bfd
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-11-14 20:51:54 +0900

    Fix build.sh

--- client/attiude-est/buildx.sh
+++ client/attiude-est/buildx.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-# /usr/local/bin/docker buildx build . -t tetsuzawa/ahrs-pi: --platform=linux/arm/v7 --no-cache=true
 /usr/local/bin/docker buildx build . -t tetsuzawa/ahrs-pi:${1} --platform linux/amd64,linux/arm64,linux/arm/v7 --push
-#/usr/local/bin/docker push tetsuzawa/ahrs-pi:${1}
